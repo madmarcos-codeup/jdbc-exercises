d8fa788badc09694010413b758677879413290d0
Mark Robinson <mark.robinson@codeup.com>
2023-08-01 12:23:33 -0500

    config done

--- src/main/java/docrob/Main.java
+++ src/main/java/docrob/Main.java
@@ -1,7 +1,46 @@
 package docrob;
 
+import com.mysql.cj.jdbc.Driver;
+import docrob.database.Config;
+
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection connection = null;
+        try {
+            // create the connection
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    Config.getUrl(),
+                    Config.getUser(),
+                    Config.getPassword()
+            );
+            String selectQuery = "SELECT * FROM albums";
+
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(selectQuery);
+
+            while (rs.next()) {
+                System.out.println("Here's an album:");
+                System.out.println("  id: " + rs.getLong("id"));
+                System.out.println("  artist: " + rs.getString("artist"));
+                System.out.println("  name: " + rs.getString("name"));
+            }
+
+        } catch(SQLException e) {
+            System.out.println("An exception happened using the connection: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // ALWAYS close the connection
+            if(connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println("An exception happened closing the connection: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
